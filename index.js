f4d9e86d25209374126f97abcf92499fec18730c
James Craine <jamesdcraine@gmail.com>
2020-02-28 11:50:18 -0500

    fixed error

--- index.js
+++ index.js
@@ -5,18 +5,21 @@ const consoleTable = require("console.table");
 const connection = mysql.createConnection({
   host: "localhost",
 
-  port: 8080,
+  port: 3306,
   user: "root",
   password: "root",
   database: "employeeTrackerDB"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  runSearch();
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+  initDatabase();
 });
 
-initDatabase();
+
 
 // first prompt
 
@@ -27,21 +30,18 @@ function initDatabase() {
               name: "trackerApp",
               type: "rawlist",
               message: "Choose function:",
-              choices: ["add_info", "view_info", "update_info", "delete_info", "close"],
+              choices: ["add_info", "view_info", "update_info", "close"],
           }
           ).then(function ({ trackerApp }) {
             switch (trackerApp) {
                   case "add_info":
-                    addInfo();
+                    addInfo()
                     break;
                   case "view_info":
                     viewInfo()
                     break;              
                   case "update_info":
                     updateInfo()
-                    break;
-                  case "delete_info":
-                    deleteInfo()
                     break;
                   case "close":                 
                     connection.end()
@@ -50,7 +50,7 @@ function initDatabase() {
         })
       }
 
-// add info function
+// // add info function
 
 function addInfo() {
   inquirer
@@ -58,13 +58,13 @@ function addInfo() {
       {
           name: "add_info",
           message: "What would you like to add?",
-          type: "list",
+          type: "rawlist",
           choices: ["employees", "roles", "departments"],
       }
   ).then(function ({ add_info }) {
       switch (add_info) {
             case "employees":
-              addEmployee();
+              addEmployee()
               break;
             case "roles":
               addRole()
@@ -76,7 +76,7 @@ function addInfo() {
   })
 }
 
-// view info function
+// view info menu
 
 function viewInfo() {
   inquirer
@@ -84,13 +84,13 @@ function viewInfo() {
       {
           name: "view_info",
           message: "What would you like to view?",
-          type: "list",
+          type: "rawlist",
           choices: ["employees", "roles", "departments"],
       }
   ).then(function ({ view_info }) {
       switch (view_info) {
             case "employees":
-              viewEmployee();
+              viewEmployee()
               break;
             case "roles":
               viewRole()
@@ -102,7 +102,7 @@ function viewInfo() {
   })
 }
 
-// update info function
+// update info menu
 
 function updateInfo() {
   inquirer
@@ -110,13 +110,13 @@ function updateInfo() {
       {
           name: "update_info",
           message: "What would you like to update?",
-          type: "list",
+          type: "rawlist",
           choices: ["employees", "roles", "departments"],
       }
   ).then(function ({ update_info }) {
       switch (update_info) {
             case "employees":
-              updateEmployee();
+              updateEmployee()
               break;
             case "roles":
               updateRole()
@@ -128,152 +128,121 @@ function updateInfo() {
   })
 }
 
-// delete info function
+// // add employee function
 
-function deleteInfo() {
-  inquirer
-  .prompt(
-      {
-          name: "delete_info",
-          message: "What would you like to delete?",
-          type: "list",
-          choices: ["employees", "roles", "departments"],
-      }
-  ).then(function ({ delete_info }) {
-      switch (delete_info) {
-            case "employees":
-              deleteEmployee();
-              break;
-            case "roles":
-              deleteRole()
-              break;              
-            case "departments":
-              deleteDepartment()
-              break;
-      }
-  })
-}
-
-// add employee function
-
-function addEmployee() {
-  inquirer
-  .prompt([
-    {
-      name: "first",
-      type: "input",
-      message: "Input new employee's first name"
-    },
-    {
-      name: "last",
-      type: "input",
-      message: "Input new employee's last name"
-    },
-    {
-      name: "role",
-      type: "input",
-      message: "Input new employee's role"
-    },
-    {
-      name: "manager",
-      type: "input",
-      message: "Input new employee's manager", 
-      validate: function(value) {
-        if (isNaN(value) === false) {
-          return true;
-        }
-        return false;
-      }
-    }
-  ])
-  .then(function(answer) {
-    connection.query(
-      "INSERT INTO employees SET ?",
-      {
-        first: answer.first,
-        last: answer.last,
-        role: answer.role,
-        manager: answer.manager
-      })
-      function(err) {
-        if (err) throw err;
-        console.log("New employee info recorded successfully");
-        initDatabase();
-      }
-    );
-  });
-}
+// function addEmployee() {
+//   inquirer
+//   .prompt([
+//     {
+//       name: "first",
+//       type: "input",
+//       message: "Input new employee's first name"
+//     },
+//     {
+//       name: "last",
+//       type: "input",
+//       message: "Input new employee's last name"
+//     },
+//     {
+//       name: "role",
+//       type: "input",
+//       message: "Input new employee's role"
+//     },
+//     {
+//       name: "manager",
+//       type: "input",
+//       message: "Input new employee's manager", 
+//       validate: function(value) {
+//         if (isNaN(value) === false) {
+//           return true;
+//         }
+//         return false;
+//       }
+//     }
+//   ])
+//   .then(function(answer) {
+//     connection.query(
+//       "INSERT INTO employees SET ?",
+//       {
+//         first: answer.first,
+//         last: answer.last,
+//         role: answer.role,
+//         manager: answer.manager
+//       })
+//       function(err) {
+//         if (err) throw err;
+//         console.log("New employee info recorded successfully");
+//         initDatabase();
+//       }
+//     );
+//   });
+// }
 
 // add role function
 
-function addRole() {
-  inquirer
-  .prompt([
-    {
-      name: "role",
-      type: "input",
-      message: "Input new role's name"
-      },
-      name: "salary",
-      type: "input",
-      message: "Input new role's salary", 
-      validate: function(value) {
-        if (isNaN(value) === false) {
-          return true;
-        }
-        return false;
-      }
-    }
-  ])
-  .then(function(answer) {
+// function addRole() {
+//   inquirer
+//   .prompt([
+//     {
+//       name: "role",
+//       type: "input",
+//       message: "Input new role's name"
+//       },
+//       {
+//       name: "salary",
+//       type: "input",
+//       message: "Input new role's salary", 
+//       validate: function(value) {
+//         if (isNaN(value) === false) {
+//           return true;
+//         }
+//         return false;
+//       }
+//     }
+//   ])
+//   .then(function(answer) {
 
-    connection.query(
-      "INSERT INTO roles SET ?",
-      {
-        role_name: answer.new_role,      
-        salary: answer.salary
-      }
-      function(err) {
-        if (err) throw err;
-        console.log("New role info recorded successfully");
-        initDatabase();
-      }
-    );
-  });
-}
+//     connection.query(
+//       "INSERT INTO roles SET ?",
+//       {
+//         role_name: answer.new_role,      
+//         salary: answer.salary
+//       })
+//       function(err) {
+//         if (err) throw err;
+//         console.log("New role info recorded successfully");
+//         initDatabase();
+//       }
+//     );
+//   });
+// }
 
-// add departments function
+// // add departments function
 
-function addDepartment() {
-  inquirer
-  .prompt([
-    {
-      name: "new_department",
-      type: "input",
-      message: "Input new department's name", 
-      validate: function(value) {
-        if (isNaN(value) === false) {
-          return true;
-        }
-        return false;
-      }
-    }
-  ])
-  .then(function(answer) {
-
-    connection.query(
-      "INSERT INTO departments SET ?",
-      {
-        department_name: answer.new_department,      
-      }
-      function(err) {
-        if (err) throw err;
-        console.log("New department info recorded successfully");
-        initDatabase();
-      }
-    );
-  });
-}
+// function addDepartment() {
+//   inquirer
+//   .prompt([
+//     {
+//       name: "new_department",
+//       type: "input",
+//       message: "Input new department's name", 
+//       validate: function(value) {
+//         if (isNaN(value) === false) {
+//           return true;
+//         }
+//         return false;
+//       }
+//     }
+//   ])
+//   .then(function ({ name }) {
+//     connection.query(`INSERT INTO department (name) VALUES ('${name}')`, function (err, data) {
+//       if (err) throw err;
+//       console.log("New department info recorded successfully");
+//         initDatabase();
+//       }
+//     );
+//   });
+// }
 
 // view employee function
 
@@ -305,71 +274,74 @@ function viewEmployee() {
     });
 }
 
-// view role function
+// // view role function
 
-function viewRole() {
-  inquirer
-    .prompt({
-      name: "role",
-      type: "input",
-      message: "Show all roles?"
-    })
-    .then(answer => {
-      let queryString = "SELECT * FROM roles";
-      let query = connection.query(queryString, { mployee: answer.employee }, (err, res) => {
+// function viewRole() {
+//   inquirer
+//     .prompt({
+//       name: "role",
+//       type: "input",
+//       message: "Show all roles?"
+//     })
+//     .then(answer => {
+//       let queryString = "SELECT * FROM roles";
+//       let query = connection.query(queryString, { mployee: answer.employee }, (err, res) => {
 
-        if (err) throw err;
+//         if (err) throw err;
 
-        res.forEach(dataRow => {
-          console.log(
-            `Employee: ${dataRow.first}${dataRow.last} || Role: ${dataRow.role} || Manager: ${dataRow.manager}` 
-          )
-        });
+//         res.forEach(dataRow => {
+//           console.log(
+//             `Employee: ${dataRow.first}${dataRow.last} || Role: ${dataRow.role} || Manager: ${dataRow.manager}` 
+//           )
+//         });
 
-        console.log(query.sql);
+//         console.log(query.sql);
 
-        console.log("Take another action");
-        initDatabase();
-      });
+//         console.log("Take another action");
+//         initDatabase();
+//       });
 
-    });
+//     });
 
-// view department function
+// // view department function
 
-function viewDepartment() {
-  inquirer
-    .prompt({
-      name: "role",
-      type: "input",
-      message: "Show all departments?"
-    })
-    .then(answer => {
-      let queryString = "SELECT * FROM departments";
-      let query = connection.query(queryString, { employee: answer.employee }, (err, res) => {
+// function viewDepartment() {
+//   inquirer
+//     .prompt({
+//       name: "role",
+//       type: "input",
+//       message: "Show all departments?"
+//     })
+//     .then(answer => {
+//       let queryString = "SELECT * FROM departments";
+//       let query = connection.query(queryString, { employee: answer.employee }, (err, res) => {
 
-        if (err) throw err;
+//         if (err) throw err;
 
-        res.forEach(dataRow => {
-          console.log(
-            `Employee: ${dataRow.first}${dataRow.last} || Role: ${dataRow.role} || Manager: ${dataRow.manager}` 
-          )
-        });
+//         res.forEach(dataRow => {
+//           console.log(
+//             `Employee: ${dataRow.first}${dataRow.last} || Role: ${dataRow.role} || Manager: ${dataRow.manager}` 
+//           )
+//         });
 
-        console.log(query.sql);
-        console.log("Take another action");
-        initDatabase();
-      });
+//         console.log(query.sql);
+//         console.log("Take another action");
+//         initDatabase();
+//       });
+//     });
+// }
+// }
 
-    });
-}
-}
+// // update employee function
+
+// function updateEmployee() {}
 
 
-function updateEmployee() {}
-function updateRole() {}
-function updateDepartment() {}
+// // update role function
 
-function deleteEmployee() {}
-function deleteRole() {}
-function deleteDepartment() {}
+// function updateRole() {}
 
+
+// // update department function
+
+// function updateDepartment() {}
