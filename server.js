41ceda44b007abcd59d9c2adb1965468d1548e9d
James Craine <jamesdcraine@gmail.com>
2020-05-04 12:42:46 -0400

    now fully functioning

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
-  database: "EmployeeTrackerDB"
+  password: "root",
+  database: "employeestrackerdb"
 });
 
 // connect to the mysql server and sql database
